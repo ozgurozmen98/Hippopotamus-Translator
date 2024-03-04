086bec6775e0c1c21e550604e2b57a06d12c5a20
Ozgur Ozmen <87929358+ozgurozmen98@users.noreply.github.com>
2024-03-04 21:33:33 +0300

    Add files via upload

---
+++ Hippopotamus Translator.py
@@ -0,0 +1,27 @@
+# enter text
+input1 = input("What's the crack? ")
+
+#change step by step
+input1 = input1.replace("e","o")
+input1 = input1.replace("i","o")
+input1 = input1.replace("ı","o")
+input1 = input1.replace("u","o")
+input1 = input1.replace("ü","o")
+input1 = input1.replace("ö","o")
+input1 = input1.replace("a","o")
+input1 = input1.replace("E","o")
+input1 = input1.replace("İ","o")
+input1 = input1.replace("I","o")
+input1 = input1.replace("U","o")
+input1 = input1.replace("Ü","o")
+input1 = input1.replace("Ö","o")
+input1 = input1.replace("A","o")
+
+print(input1)
+
+
+
+
+
+
+
